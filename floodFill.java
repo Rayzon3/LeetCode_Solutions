72e453a6398333d840f4d063407ccce69e87cb8b
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2021-12-03 07:10:05 +0530

    pushed

---
+++ floodFill.java
@@ -0,0 +1,30 @@
+//dfs
+
+class floodFillSolution {
+
+    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor){
+        if(image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
+    public static void fill(int[][] image, int sr, int sc, int oldColor, int newColor){
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image.length || image[sr][sc] != oldColor){
+            return;
+        }
+        image[sr][sc] = newColor;
+        fill(image, sr - 1, sc, oldColor, newColor); //top
+        fill(image, sr + 1, sc, oldColor, newColor); //bottom
+        fill(image, sr, sc - 1, oldColor, newColor); //left
+        fill(image, sr, sc + 1, oldColor, newColor); //right
+    }
+
+    public static void main(String[] args) {
+        int[][] image = {{1,1,1},{1,1,0},{1,0,1}};
+        int sr = 1;
+        int sc = 1;
+        int newColor = 2;
+
+        floodFill(image, sr, sc, newColor);
+    }
+}
