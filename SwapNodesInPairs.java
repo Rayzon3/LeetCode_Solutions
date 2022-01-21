1ee2c3eef00e829e4003fabed0fde89088960b13
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2022-01-21 05:48:43 +0530

    pushed

---
+++ SwapNodesInPairs.java
@@ -0,0 +1,28 @@
+public class swapeNodesInPairs {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        };
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode temp = head.next;
+        head.next = swapPairs(head.next.next);
+        temp.next = head;
+        return temp;
+    }
+}
